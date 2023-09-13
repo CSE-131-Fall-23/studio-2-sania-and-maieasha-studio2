144bd38f974a6c8a6c7c2aab8d5f69692852bc93
saniaali <saniaali@sanias-air.dhcp.wustl.edu>
2023-09-13 15:50:18 -0500

    ok

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -1,40 +1,40 @@
-package studio2;
-
-import java.util.Scanner;
-
-public class Ruin {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Scanner in = new Scanner(System.in);
-		// 
-	System.out.println("Beginning Amount of Money");
-	int startmoney= in.nextInt();
-	
-	System.out.println("Win Probability:");
-	double winprobability= in.nextDouble();
-			
-	System.out.println("win Limit:");
-	int winlimit= in.nextInt();
-	{	
-		
-	}
-
-
-	if (startmoney >  "0" && startmoney)
-	{ System.out.println("stop");
-	
-	}
-	
-	else 
-	{
-	System.out.println("keep playing");}
-	
-	} }
-	
-    
-	
-	
-
-
+//package studio2;
+//
+//import java.util.Scanner;
+//
+//public class Ruin {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		
+//		Scanner in = new Scanner(System.in);
+//		// 
+//	System.out.println("Beginning Amount of Money");
+//	int startmoney= in.nextInt();
+//	
+//	System.out.println("Win Probability:");
+//	double winprobability= in.nextDouble();
+//			
+//	System.out.println("win Limit:");
+//	int winlimit= in.nextInt();
+//	{	
+//		
+//	}
+//
+//
+//	if (startmoney >  "0" && startmoney)
+//	{ System.out.println("stop");
+//	
+//	}
+//	
+//	else 
+//	{
+//	System.out.println("keep playing");}
+//	
+//	} }
+//	
+//    
+//	
+//	
+//
+//
